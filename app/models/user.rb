7e3886331b875bd782a0a838e271f243bf0b709e
ponyuta <58424732+ponyuta@users.noreply.github.com>
2019-12-25 22:32:19 +0900

    createmodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   has_many :likes
+
+  has_many :comments
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
